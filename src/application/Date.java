564f5f7485dae528b37ae53d71a3eace735e6b2b
programmerBrandon <100105534+programmerBrandon@users.noreply.github.com>
2023-08-05 22:43:55 -0400

    0.3.0-alpha code update
    Did more coding for pre-release version 0.3.0, work done includes some work on the methods in the Save class (including being able to open Save Dialog Box), added validation checks to the title textfield on the game setup page in order to prevent characters that are not allowed on the file path if title is used as part of a file. Also modified the validation check for the number of players textfield in GameSetupController class in order to prevent it from clashing/overriding the checks to the title field. 
    
    More work to come soon!

--- src/application/Date.java
+++ src/application/Date.java
@@ -9,20 +9,36 @@ public class Date {
 
 	Date() {
 		date = LocalDate.now();
-		//System.out.println("Date: " + date);
+		//System.out.println("FIXME: Date: " + date); //FIXME
 	}
 	
+	/**
+	 * Method that returns the year (as set by the user's device).
+	 * @returns a String with the current year.
+	 */
 	public String getYear() {
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy"); 
 		
-		String formattedDate = date.format(format);
-		return formattedDate;
+		return date.format(format);
 	}
 	
+	/**
+	 * A method that returns the date in USA Format with slashes as separator (MM/dd/yyyy).
+	 * @returns A String of the date in USA Format, separated by slashes.
+	 */
 	public String getDateUSFormat() {
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy"); 
 		
-		String formattedDate = date.format(format);
-		return formattedDate;
+		return date.format(format);
+	}
+	
+	/**
+	 * A method that returns the date in USA Format with dashes as separator (MM-dd-yyyy).
+	 * @returns A String of the date in USA Format, separated by dashes.
+	 */
+	public String getDateUSFormatDashes() {
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("MM-dd-yyyy"); 
+		
+		return date.format(format);
 	}
 }
--- src/application/Save.java
+++ src/application/Save.java
@@ -1,12 +1,31 @@
 package application;
 
+//import java.awt.Window;
 import java.io.File;
+import javafx.stage.Window;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.FileChooser;
 
 
 public class Save {
+	Date date = new Date();
 	
 	Save() {
 		//TODO
+	}
+	
+	public void openSaveDialog(Window window) {
+		FileChooser fileChooser = new FileChooser();
+		System.out.println("FIXME: defaultFileName(): " + defaultFileName()); //FIXME
+ 		fileChooser.setInitialFileName(defaultFileName());
+		FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Text Document (*.txt)", "*.txt");
+		fileChooser.getExtensionFilters().add(filter);
+		File file = fileChooser.showSaveDialog(window);
+		System.out.println("FIXME: file: " + file.getName());
+		System.out.println("FIXME: file path: " + file.getPath());
+		
+		checkForErrors(file);
 	}
 	
 	public void createFile(File file) {
@@ -16,6 +35,48 @@ public class Save {
 	
 	private void writeResultsToFile(File file) {
 		//TODO
+	}
+	
+	private String sanitizeFileName(String string) {
+		//TODO
+		return ""; //FIXME
+	}
+	
+	private void renameFile(File file, String name) {
+		//TODO
+	}
+	
+	public String defaultFileName() {
+		if(GameData.getGameTitle().equals("")) {
+			return "Name That Tune - " + date.getDateUSFormatDashes() + ".txt";
+		}
+		
+		else {
+			return "Name That Tune - " + GameData.getGameTitle() + ".txt";
+		}
+	}
+	
+	public void checkForErrors(File file) {
+		if(file == null) {
+			System.out.println("Could not save game because file is null!");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Error: Could not save results because no valid file was selected or the dialog box was closed without pressing save.");
+			alert.show();
+		}
+		
+		if(!file.toString().endsWith(".txt")) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Error: Could not save results because file extension is invalid. Please make sure to only save as .txt");
+			alert.show();
+		}
+		
+		
+		
+		else {
+			createFile(file);
+			System.out.println("FIXME: No Errors Detected With file.");
+		}
+		
 	}
 
 }
